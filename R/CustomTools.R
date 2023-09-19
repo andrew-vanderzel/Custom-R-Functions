3c6e0c3b0235919f0debc4c0e2c0853c624de72d
Andrew Van Der Zel <35973970+andrew-vanderzel@users.noreply.github.com>
2023-09-19 09:44:40 -0400

    Added import github function

--- R/CustomTools.R
+++ R/CustomTools.R
@@ -13,8 +13,18 @@ ViewExcel <- function(df = .Last.value, file = tempfile(fileext = ".csv")) {
 #' Provides a janitor distribution table but with formatting and ordering by default
 #' @export
 f_tbyl <- function(data, vbl){
+  library(dplyr)
+  library(janitor)
   data %>%
     tabyl({{vbl}}) %>%
     adorn_pct_formatting() %>%
     arrange(desc(n))
+}
+
+#' Function to import github package with updated download setting
+#' @export
+import_github <- function(path){
+  library(devtools)
+  options(download.file.method = "wininet")
+  devtools::install_github(path)
 }
